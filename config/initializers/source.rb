a122c58de6530c05aa52e7a6afb4781795ded562
David Yip <yipdw@member.fsf.org>
2017-10-18 02:13:43 -0500

    Link to source repo.

---
+++ config/initializers/source.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+module Mastodon
+  module Version
+    module_function
+    def source_base_url
+      'https://gitlab.peach-bun.com/yipdw/mastodon'
+    end
+  end
+end
